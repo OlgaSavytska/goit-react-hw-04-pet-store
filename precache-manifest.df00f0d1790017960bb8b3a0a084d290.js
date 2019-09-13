0caab64f9c5e56efbc4b79b516d367085126aa91
OlgaSavytska <o.savytska77@gmail.com>
2019-09-13 18:26:00 +0300

    Updates

---
+++ precache-manifest.df00f0d1790017960bb8b3a0a084d290.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b82381cf61eee80d5d6ac9d381d2e3ee",
+    "url": "/goit-react-hw-04-pet-store/index.html"
+  },
+  {
+    "revision": "0e93316314c44157ee80",
+    "url": "/goit-react-hw-04-pet-store/static/css/main.bfadd3b9.chunk.css"
+  },
+  {
+    "revision": "478810c47d50d9f5c428",
+    "url": "/goit-react-hw-04-pet-store/static/js/2.8183fc0b.chunk.js"
+  },
+  {
+    "revision": "0e93316314c44157ee80",
+    "url": "/goit-react-hw-04-pet-store/static/js/main.26aeded9.chunk.js"
+  },
+  {
+    "revision": "249656bcd6aee9fe97fe",
+    "url": "/goit-react-hw-04-pet-store/static/js/runtime~main.16247d91.js"
+  }
+]);
